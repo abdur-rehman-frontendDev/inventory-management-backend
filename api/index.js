6040e2e0185d2e9511f88806514eb185b288cddb
Abdur Rehman <ab.rehman2534@gmail.com>
2026-07-07 11:34:12 +0500

    Initial commit after detached forked

---
+++ api/index.js
@@ -0,0 +1,52 @@
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env")
+});
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const { MongoDBconfig } = require("../libs/mongoconfig");
+
+const authrouter = require("../Routers/authRouther");
+const productrouter = require("../Routers/ProductRouter");
+const orderrouter = require("../Routers/orderRouter");
+const categoryrouter = require("../Routers/categoryRouter");
+const notificationrouter = require("../Routers/notificationRouters");
+// const activityrouter = require("../Routers/activityRouter");
+const inventoryrouter = require("../Routers/inventoryRouter");
+const salesrouter = require("../Routers/salesRouter");
+const supplierrouter = require("../Routers/supplierrouter");
+const stocktransactionrouter = require("../Routers/stocktransactionrouter");
+
+const app = express();
+
+MongoDBconfig();
+
+app.use(
+  cors({
+    origin: ["https://mumeez-inventory.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+
+app.use("/api/auth", authrouter);
+app.use("/api/product", productrouter);
+app.use("/api/order", orderrouter);
+app.use("/api/category", categoryrouter);
+app.use("/api/notification", notificationrouter);
+// app.use("/api/activitylogs", activityrouter);
+app.use("/api/inventory", inventoryrouter);
+app.use("/api/sales", salesrouter);
+app.use("/api/supplier", supplierrouter);
+app.use("/api/stocktransaction", stocktransactionrouter);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
+module.exports = app;
--- libs/mongoconfig.js
+++ libs/mongoconfig.js
@@ -1,15 +1,46 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
+let cached = global.mongoose;
 
-require("dotenv").config()
-
-module.exports.MongoDBconfig=()=>{
-    mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>{
-        console.log("connected to database successfully")
-    })
-    .catch((err)=>{
-        console.log("MonogoDB Connection Error",err)
-    })
-
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+  };
 }
+
+async function MongoDBconfig() {
+  if (!process.env.MONGODB_URL) {
+    throw new Error("MONGODB_URL is missing in .env");
+  }
+
+  try {
+    if (cached.conn) {
+      return cached.conn;
+    }
+
+    cached.conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log("MongoDB Connected");
+
+    return cached.conn;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = { MongoDBconfig };
+
+// const mongoose=require("mongoose")
+
+// require("dotenv").config()
+
+// module.exports.MongoDBconfig=()=>{
+//     mongoose.connect(process.env.MONGODB_URL)
+//     .then(()=>{
+//         console.log("connected to database successfully")
+//     })
+//     .catch((err)=>{
+//         console.log("MonogoDB Connection Error",err)
+//     })
+
+// }
