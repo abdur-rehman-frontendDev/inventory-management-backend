fbfe1a1486c2c0dc32afac311f27a9bae6fcd206
Abdur Rehman <ab.rehman2534@gmail.com>
2026-07-07 12:29:40 +0500

    Bug fixed

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 require("dotenv").config({
-  path: path.resolve(__dirname, "../.env")
+  path: path.resolve(__dirname, "../.env"),
 });
 
 const express = require("express");
@@ -28,6 +28,7 @@ app.use(
   cors({
     origin: ["https://mumeez-inventory.vercel.app", "http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
 );
 
