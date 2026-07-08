6471a934f50c20da2272610f090e67164986d373
Abdur Rehman <ab.rehman2534@gmail.com>
2026-07-08 12:01:35 +0500

    Bug fixed

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -73,14 +73,6 @@ module.exports.login = async (req, res) => {
 
     const token = await generateToken(duplicatedUser, res);
 
-    // await logActivity({
-    //   action: "User Login",
-    //   description: `User ${duplicatedUser.name} logged in.`,
-    //   entity: "user",
-    //   entityId: duplicatedUser._id,
-    //   userId: duplicatedUser._id,
-    //   ipAddress: ipAddress,
-    // });
     return res.status(201).json({
       message: "login successfully",
       user: {
