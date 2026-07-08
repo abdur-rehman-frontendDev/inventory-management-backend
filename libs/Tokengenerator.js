2921f53991060ecc891d28adf8cfb8873325a13e
Abdur Rehman <ab.rehman2534@gmail.com>
2026-07-08 15:45:56 +0500

    Bug fixed

--- libs/Tokengenerator.js
+++ libs/Tokengenerator.js
@@ -1,59 +1,59 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = async (user, res) => {
-  const token = jwt.sign(
-    {
-      userId: user._id,
-      role: user.role,
-    },
-    process.env.SecretKey,
-    {
-      expiresIn: "7d",
-    }
-  );
-
-  res.cookie("Inventorymanagmentsystem", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
-
-  return token;
-};
-
-module.exports = generateToken;
-
-// const jwt = require('jsonwebtoken');
-
-// require('dotenv').config();
-
+// const jwt = require("jsonwebtoken");
 
 // const generateToken = async (user, res) => {
-//   try {
-//     if (!process.env.SecretKey) {
-//       throw new Error("Secret key is not defined in the environment variables.");
+//   const token = jwt.sign(
+//     {
+//       userId: user._id,
+//       role: user.role,
+//     },
+//     process.env.SecretKey,
+//     {
+//       expiresIn: "7d",
 //     }
+//   );
 
-//     const token = jwt.sign(
-//       { userId: user._id, role: user.role },
-//       process.env.SecretKey,
-//       { expiresIn: '7d' }
-//     );
+//   res.cookie("Inventorymanagmentsystem", token, {
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: "None",
+//     maxAge: 7 * 24 * 60 * 60 * 1000,
+//   });
 
-//     res.cookie("Inventorymanagmentsystem", token, {
-//       maxAge: 7 * 24 * 60 * 60 * 1000,
-//       httpOnly: true,
-//       sameSite: 'None',
-//       secure: true,
-//     });
-    
-
-//     return token; 
-//   } catch (error) {
-//     console.error("Error generating token:", error.message);
-//     throw new Error("Failed to generate token");
-//   }
+//   return token;
 // };
 
-// module.exports=generateToken;
+// module.exports = generateToken;
+
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+
+const generateToken = async (user, res) => {
+  try {
+    if (!process.env.SecretKey) {
+      throw new Error("Secret key is not defined in the environment variables.");
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.SecretKey,
+      { expiresIn: '7d' }
+    );
+
+    res.cookie("Inventorymanagmentsystem", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+    });
+    
+
+    return token; 
+  } catch (error) {
+    console.error("Error generating token:", error.message);
+    throw new Error("Failed to generate token");
+  }
+};
+
+module.exports=generateToken;
